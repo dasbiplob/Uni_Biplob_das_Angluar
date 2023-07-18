1dc4bc484d0c3164a82ad20f3fee4540cbfdb647
dasbiplob <biplob90.d@git.com>
2023-07-19 00:56:32 +0300

    Adding component

--- cwh-todo-list/src/app/MyComponent/todos/todos.component.ts
+++ cwh-todo-list/src/app/MyComponent/todos/todos.component.ts
@@ -7,37 +7,45 @@ import { Todo } from '../../Todo';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent {
+  "localItems":string;
   "todos": Todo[];
   constructor(){
-    this.todos = [
-        {
-          sno:1,
-          title: "This is Title1",
-          desc: "Description",
-          active: true
-        },
-        {
-          sno:2,
-          title: "This is Title2",
-          desc: "Description",
-          active: true
-        },
-        {
-          sno:3,
-          title: "This is Title3",
-          desc: "Description",
-          active: true
-        },
-    ]
+    this.localItems = JSON.parse(localStorage.getItem("todos") || '{}');
+    if(this.localItems==null){
+
+      this.todos = [];
+    }else{
+      this.todos = JSON.parse(this.localItems);
+    }
+        // {
+        //   sno:1,
+        //   title: "This is Title1",
+        //   desc: "Description",
+        //   active: true
+        // },
+        // {
+        //   sno:2,
+        //   title: "This is Title2",
+        //   desc: "Description",
+        //   active: true
+        // },
+        // {
+        //   sno:3,
+        //   title: "This is Title3",
+        //   desc: "Description",
+        //   active: true
+        // },
   }
 
   deleteTodo(todo: Todo){
     console.log(todo);
     const index = this.todos.indexOf(todo);
-    this.todos.slice(index,1)
+    this.todos.slice(index,1);
+    localStorage.setItem("todos",JSON.stringify(this.todos));
   }
   addTodo(todo: Todo){
     console.log(todo);
     this.todos.push(todo);
+    localStorage.setItem("todos",JSON.stringify(this.todos));
   }
 }
